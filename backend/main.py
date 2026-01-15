47906630541921745e27c6baf6cebf5cf67e01fc
Mohit Chawla <mohit.c@consultadd.com>
2026-01-15 22:49:44 +0530

    main pipeline start

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,24 @@
 # pipeline
 from utils.llm import llm_query, llm_structured
+from utils.transcription import get_word_array
+from utils.videoProcessing import get_video_dimensions, extract_audio_for_asr, extract_audio, trim_video
+
+def process_video(file_path,upload=False):
+
+    # contextAwareCrop(video_path)
+
+    width, height = get_video_dimensions(file_path)
+
+    audio_path = extract_audio_for_asr(file_path)
+
+    word_array = get_word_array(audio_path)
+
+    script = ""
+    for word in word_array:
+        script += f"{word} "
+
+    # trimmed_path = trim_video(file_path, start_time=0, end_time=60)
+
+    llm_structured()
+
 
